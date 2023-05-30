e2b5101e2cbf99941bef2a60d19de2b5f35df2b7
ychaaban2004 <youssefchaabani123@gmail.com>
2023-05-29 23:04:40 -0600

    terrorism

--- com/craftinginterpreters/trick/trick.java
+++ com/craftinginterpreters/trick/trick.java
@@ -1,6 +1,7 @@
 package com.craftinginterpreters.trick;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
@@ -18,5 +19,16 @@ public class trick{
         } else {
             runPrompt();
         }
+    }
+
+    private static void runFile(String path) throws IOException {
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        run(new String(bytes,Charset.defaultCharset()));
+    }
+
+    private static void runPrompt() throws IOException{
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
+
     }
 }
